44fd6f44ca29063b7a023dd540a3df2b4f724d92
Talha Öztürk <talhaozturk@Mac.fritz.box>
2026-09-20 18:53:23 +0200

    update resend from address

--- backend/services/mailer.js
+++ backend/services/mailer.js
@@ -1,7 +1,7 @@
 const { Resend } = require('resend');
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-const FROM = 'Goldpark Management <onboarding@resend.dev>';
+const FROM = 'Goldpark Management <noreply@goldpark-fuhrparkmanagement.de>';
 const ADMIN_EMAIL = 'info@goldpark-fuhrparkmanagement.de';
 
 async function sendWelcomeMail(kundeEmail, kundenName, firmaName) {
